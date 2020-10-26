a4997c8a92da91a940f863f01da22fcde312d427
gutandrHSLU <71374210+gutandrHSLU@users.noreply.github.com>
2020-10-26 08:04:06 +0100

    Update Testat1_Aufgabe2_AddBlocks.py
    Updated Version

--- Testat1_Aufgabe2_AddBlocks.py
+++ Testat1_Aufgabe2_AddBlocks.py
@@ -5,32 +5,21 @@ import random
 RDK = Robolink()
 
 global numberOfBlocks
-numberOfBlocks = 26
+numberOfBlocks = 26 #Startwert welcher dann mit dem Fenster angepasst werden kann.
 
-f_place = RDK.Item('tower')
-t_home = RDK.Item('home')
-f_world = RDK.Item('world')
 blocks = []
 
 
 def RunProgram(numberOfBlocks):
     f_blocks = RDK.Item("blocks", ITEM_TYPE_FRAME)
-    newBlock = RDK.AddFile(r'C:\Users\aanng\OneDrive - Hochschule Luzern\5.Semester\AROB\Aufgaben\Testat\block.IGS', f_blocks)
-    newBlock.Copy()
-    newBlock.setName("block1")
-    newBlock.setPose(transl(20, 65, 7.75) * rotz(90*pi/180))
-    newBlock.setParent(f_blocks)
-    newBlock.Recolor([0, 0.5, 1])
-    blocks.append(newBlock)
-    for i in range(2, numberOfBlocks+1):
-        newItem = RDK.Paste()
-        newItem.setParent(f_blocks)
-        newItem.Recolor([1/numberOfBlocks*i, 0.5, 1-(1/numberOfBlocks*i)])
+    for i in range(1, numberOfBlocks+1):
+        newBlock = RDK.AddFile(r'C:\Users\aanng\OneDrive - Hochschule Luzern\5.Semester\AROB\Aufgaben\Testat\block.IGS', f_blocks)
+        newBlock.Recolor([1/numberOfBlocks*i, 0.5, 1-(1/numberOfBlocks*i)])
         if i%2:
-            newItem.setPose(transl(20 + (i-1)*15, 65, 7.75) * rotz(90*pi/180))
+            newBlock.setPose(transl(20 + (i-1)*15, 65, 7.75) * rotz(90*pi/180))
         else:
-            newItem.setPose(transl(20 + (i-2)*15, 190, 7.75) * rotz(90*pi/180))
-        blocks.append(newItem.setName("block" + str(i)))
+            newBlock.setPose(transl(20 + (i-2)*15, 190, 7.75) * rotz(90*pi/180))
+        blocks.append(newBlock.setName("block" + str(i)))
     RDK.Finish()
 
 
@@ -50,9 +39,9 @@ Entry(root, textvariable=entry_speed).pack()
 
 # Add a button and default action to execute the current choice of the user
 def ExecuteChoice():
-    SPEED_WELD = int(entry_speed.get())
+    NUMBER_OF_BLOCKS = int(entry_speed.get())
     # Run the main program once all the global variables have been set
-    RunProgram(SPEED_WELD)
+    RunProgram(NUMBER_OF_BLOCKS)
 
 Button(root, text='Add blocks', command=ExecuteChoice).pack()
 
